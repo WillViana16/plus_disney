21480a4e39fe8da713dec00acfbccba7489c943c
Willian <willianvia1@gmail.com>
2024-10-17 22:13:10 -0300

    criando faq,footer,header e outras funcionalidades

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded' , function () {
     const buttons = document.querySelectorAll('[data-tab-button]');
     const tabsConstainer = document.querySelectorAll('[data-tab-id]');
+    const questions = document.querySelectorAll('[data-faq-question]');
     
     for (let i = 0; i < buttons.length; i++ ) {
         buttons[i].addEventListener('click',  function (botao) {
@@ -13,8 +14,19 @@ document.addEventListener('DOMContentLoaded' , function () {
         })
     }
 
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreOuFechaResposta);
+    }
+
     
 })
+
+function abreOuFechaResposta(elemento) {
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
+}
 
 function removeBotaoAtivo () {
     const buttons = document.querySelectorAll('[data-tab-button]');

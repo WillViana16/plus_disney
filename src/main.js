78e68c179b1663eba7cb72a5fd59ca327000dbce
Willian <willianvia1@gmail.com>
2024-10-28 22:14:03 -0300

    Criando sessão do header, estilizando botoes, exibindo/ocultando header

--- src/main.js
+++ src/main.js
@@ -2,7 +2,23 @@ document.addEventListener('DOMContentLoaded' , function () {
     const buttons = document.querySelectorAll('[data-tab-button]');
     const tabsConstainer = document.querySelectorAll('[data-tab-id]');
     const questions = document.querySelectorAll('[data-faq-question]');
-    
+
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;// Pega a altura do elemento com clientHeight
+
+    //Responsavel por ocultar e exibir os elementos do header na pagina quando o scroll passar por uma determinada altura
+    window.addEventListener('scroll', function() {
+        const posicaoAtual = window.scrollY;
+
+        if (posicaoAtual < alturaHero ){
+            ocultaElementosHeader ();
+        } else {
+            exibeElementosHeader();
+        }
+
+        })
+
+    // Seçao de atrações, programação das abas
     for (let i = 0; i < buttons.length; i++ ) {
         buttons[i].addEventListener('click',  function (botao) {
             const abaAlvo = botao.target.dataset.tabButton;
@@ -14,12 +30,26 @@ document.addEventListener('DOMContentLoaded' , function () {
         })
     }
 
+
+    // Seção FAQ, Accordion
     for (let i = 0; i < questions.length; i++) {
         questions[i].addEventListener('click', abreOuFechaResposta);
     }
 
     
 })
+
+//classe --is-hidden está sendo adicionada no header
+function ocultaElementosHeader () {
+    const header = document.querySelector ('header');
+    header.classList.add('header--is-hidden');
+}
+
+// classe --is-hidden está sendo removida do header
+function exibeElementosHeader () {
+    const header = document.querySelector ('header');
+    header.classList.remove('header--is-hidden'); 
+}
 
 function abreOuFechaResposta(elemento) {
     const classe = 'faq__questions__item--is-open';
